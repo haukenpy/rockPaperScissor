21f6fa9e2214026dcc05d887e9b0d5397e8ac5cd
khausbak <py.hauken@gmail.com>
2024-06-03 09:00:01 +0200

    Remove temporary console.log messages, added comments to functions

--- myscript.js
+++ myscript.js
@@ -9,12 +9,12 @@ let computerChoice = function() {
 let playerChoice = function() {
     let player_choice = prompt("Select your option: ", "");
 
-    /* Cas the player selection to lowercase and ensure it is a valid option */
+    // Cast the player selection to lowercase and ensure it is a valid option
     player_choice = player_choice.toLowerCase();
     if (player_choice == "rock" || player_choice == "paper" || player_choice == "scissor") {
         return player_choice;
     } else {
-        /* If players choice is not valid, prompt error message and ask for new input */
+        // If players choice is not valid, prompt error message and ask for new input
         alert("Player choice is not valid");
         playerChoice()
     }
@@ -22,25 +22,37 @@ let playerChoice = function() {
 
 // If statement to determine the winner, computer or human depending on choices. Returns 1 if human wins, returns 2 if computer wins.
 let determineWinner = function(player_choice, computer_choice) {
+
+    // Determine if human wins. returns value 1 if true.
     if ((player_choice == "rock" && computer_choice == "scissor") ||
         (player_choice == "scissor" && computer_choice == "paper") ||
         (player_choice == "paper" && computer_choice == "rock")) {
             return 1;
         }
+    
+    // Determine if the computer wins. returns value 2 if true.
     else if ((computer_choice == "rock" && player_choice == "scissor") ||
     (computer_choice == "scissor" && player_choice == "paper") ||
     (computer_choice == "paper" && player_choice == "rock")) {
         return 2;
+
+    // If no winner, log a draw.
     } else {
         console.log("Its a draw");
     }};
 
 // Receive the human input and computer input. Run the determine winner function and update the scores. Returns 1 if human wins, returns 2 if computer wins.
 let playRound = function() {
+
+    //Retrieve the computer input.
     const computerSelection = computerChoice()
     console.log(computerSelection);
+
+    //Retrieve the human input.
     const humanSelection = playerChoice()
     console.log(humanSelection);
+
+    //Return the winner. 1 if human wins, 2 if computer wins.
     return determineWinner(humanSelection, computerSelection);
 }
 
@@ -54,7 +66,7 @@ let playGame = function() {
     let round = 0;
     while (round < 6){
 
-        /* Play a round */
+        // Play a round
         let roundWinner = playRound()
 
         // If human wins, increment the score
@@ -75,29 +87,12 @@ let playGame = function() {
         round++
     }
 
+    // Determine the game winner and display in the console. 
     let winner = (playerScore > computerScore) ? "Human won the game!" :
                  (computerScore > playerScore) ? "Computer won the game!" :
                  "Game ended in a draw!";
     console.log(winner);
 }
 
+// Run the main function to start the game.
 playGame();
-
-/* Pseudocode 
-
-Use variables to keep track of scores'
-1. Define the logic to determine the winner:
-    Rock > Scissors
-    Scissors > Paper
-    Paper > Rock
-    if Computer input == User Input, draw.
-
-2. Keep track of scores (two variables? User Score, Human score?)
-
-
-
-Write logic to play a signle round
-
-Write code to play the full game. should last 5 round.
-
-*/
